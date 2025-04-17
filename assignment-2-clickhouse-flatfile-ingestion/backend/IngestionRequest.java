e6cef1ef8bbc4c83e4166994ac2a3ba103453d40
Trinadh Katlagunta <katlaguntatrinadh37@gmail.com>
2025-04-17 14:56:27 +0530

    Create IngestionRequest.java

---
+++ assignment-2-clickhouse-flatfile-ingestion/backend/IngestionRequest.java
@@ -0,0 +1,10 @@
+package io.zeotap.backend.controller;
+
+public class IngestionRequest {
+    private String source;
+    private String target;
+    private String filePath;
+    private String columnNames;
+
+    // Getters and setters
+}
